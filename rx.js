c9c7e3982d4ca38bbdde7cc54443081242f67a40
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2023-03-09 22:26:03 -0300

    feat : Update Observable.pipe and use operators

--- rx.js
+++ rx.js
@@ -1,3 +1,4 @@
 export { Subscription } from "./src/rx/subscription.js";
 export { Observable } from "./src/rx/observable.js";
-export { Subject } from "./src/rx/subject.js";
+export { Subject, BehaviorSubject } from "./src/rx/subject.js";
+export * from "./src/rx/operators.js"
