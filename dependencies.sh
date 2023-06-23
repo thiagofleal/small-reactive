a4ca1e7705869106c0500bb5217cfe5c3eac6e81
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2023-06-22 22:37:43 -0300

    build (ugdm) : Update small-rxjs version

--- dependencies.sh
+++ dependencies.sh
@@ -5,5 +5,5 @@ git clone https://github.com/thiagofleal/small-rxjs small-rxjs
 cd small-rxjs
 git fetch https://github.com/thiagofleal/small-rxjs && git checkout master
 git pull https://github.com/thiagofleal/small-rxjs master
-git checkout 2d3e038eaf79cd3316fc0dc44bb27c80f11d6e05
+git checkout fa307589262ae93d81ffd2301285f957520b3274
 cd ..
