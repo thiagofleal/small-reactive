e2af44147580a7cf3750d9f7743be39ea157bb08
Thiago Leal <thiago.leal@alghorit.com.br>
2023-07-24 08:03:19 -0300

    feat : Update ugdm

--- dependencies.bat
+++ dependencies.bat
@@ -1,10 +1,10 @@
 echo off
 IF NOT EXIST vendor @MKDIR vendor
 CD vendor/
-git clone https://github.com/thiagofleal/small-rxjs small-rxjs
+CALL git clone https://github.com/thiagofleal/small-rxjs small-rxjs
 CD small-rxjs
-git fetch https://github.com/thiagofleal/small-rxjs
-git checkout master
-git pull https://github.com/thiagofleal/small-rxjs master
-git checkout fa307589262ae93d81ffd2301285f957520b3274
+CALL git fetch https://github.com/thiagofleal/small-rxjs
+CALL git checkout master
+CALL git pull https://github.com/thiagofleal/small-rxjs master
+CALL git checkout fa307589262ae93d81ffd2301285f957520b3274
 CD ..
