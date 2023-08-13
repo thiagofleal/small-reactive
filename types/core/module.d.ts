12496f26454699aae003a844b84e7998965c7329
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2023-08-13 18:07:14 -0300

    fix (types) : Update Module.register params

--- types/core/module.d.ts
+++ types/core/module.d.ts
@@ -13,7 +13,10 @@ export class Module {
       args?:      any
     })[]
   })
-  static register(module: Constructable<Module> | { module: Constructable<Module>, args: any }): void
+  static register(
+    module: Constructable<Module> | { module: Constructable<Module>, args?: any },
+    args?:  any
+  ): void
   static getFromComponent(component: Component): Module | undefined
   static getFromDirective(directive: Directive): Module | undefined
   inject<T extends Service>(service: Constructable<T>): T | undefined
